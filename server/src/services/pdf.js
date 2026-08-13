0070c3f56d29872cc6c0cf352793f92b13d64133
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-08-13 14:11:13 +0530

    Add PDF service

---
+++ server/src/services/pdf.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import { PDFParse } from 'pdf-parse';
+
+export async function extractPdfText(filePath) {
+  const buffer = fs.readFileSync(filePath);
+  const parser = new PDFParse({ data: buffer });
+  try {
+    const result = await parser.getText();
+    return result.text || '';
+  } finally {
+    await parser.destroy();
+  }
+}
